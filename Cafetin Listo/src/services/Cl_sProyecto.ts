8cd9a8c32245a9b77c3449c6840482f7e4e5046f
Snyder-kun <Snyderatacho@gmail.com>
2026-06-08 19:28:43 -0400

    Delete Cafetin Listo directory

--- Cafetin Listo/src/services/Cl_sProyecto.ts
+++
@@ -1,6 +0,0 @@
-import Cl_sMockApi from "./Cl_sMockApi.js";
-
-export default class Cl_sProyecto extends Cl_sMockApi {
-  protected static apiUrl: string =
-    "https://6a274314a84f9d39e9086079.mockapi.io/cafeteria/LosSteve/";
-}

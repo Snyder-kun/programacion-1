612131ebe2c61a03e2ee51e4a3bc0b616513ed3d
Snyder-kun <Snyderatacho@gmail.com>
2026-07-20 04:29:44 -0400

    Delete src directory

--- src/services/Cl_sProyecto.ts
+++
@@ -1,6 +0,0 @@
-import Cl_sMockApi from "./Cl_sMockApi.js";
-
-export default class Cl_sProyecto extends Cl_sMockApi {
-  protected static apiUrl: string =
-    "https://6a5d1df80ad09982aef6e908.mockapi.io";
-}
